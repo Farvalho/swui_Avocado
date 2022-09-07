9f67e405fd490541ac800a1229d5447e9e0a9b23
Fabio Carvalho <farvalho@outlook.com>
2022-09-07 15:37:34 +0100

    +Add: Home view layout

--- Avocado/Avocado/Presentation/Views/AvocadosView.swift
+++ Avocado/Avocado/Presentation/Views/AvocadosView.swift
@@ -8,13 +8,58 @@
 import SwiftUI
 
 struct AvocadosView: View {
+    
+    @State private var pulsateAnimation: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            
+            Image("avocado")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 240, height: 240)
+                .shadow(color: Color("ColorBlackTransparentDark"), radius: 12, x:  0, y: 8)
+                .scaleEffect(pulsateAnimation ? 1 : 0.9)
+                .opacity(pulsateAnimation ? 1 : 0.9)
+                .animation(.easeInOut(duration: 1.5).repeatForever(autoreverses: true), value: pulsateAnimation)
+            
+            VStack {
+                Text("Avocados".uppercased())
+                    .font(.system(size: 42, weight: .bold, design: .serif))
+                    .foregroundColor(.white)
+                    .padding()
+                    .shadow(color: Color("ColorBlackTransparentDark"), radius: 4, x: 0, y: 4)
+                
+                Text("Creamy, green, and full of nutrients! Avocado is a powerhouse ingredient at any meal. Enjoy these handpicked avocado recipes for breakfast, lunch, dinner & more!")
+                    .lineLimit(nil)
+                    .font(.system(.headline, design: .serif))
+                    .foregroundColor(Color("ColorGreenLight"))
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(8)
+                    .frame(maxWidth: 640, minHeight: 120)
+                    .padding(.horizontal, 10)
+                
+            }
+            
+            Spacer()
+        } //:VStack
+        .background(
+            Image("background")
+                .resizable()
+                .scaledToFill()
+        )
+        .edgesIgnoringSafeArea(.all)
+        .onAppear {
+            pulsateAnimation.toggle()
+        }
     }
 }
 
 struct AvocadosView_Previews: PreviewProvider {
     static var previews: some View {
         AvocadosView()
+            .previewDevice("iPhone 11 Pro")
+            .environment(\.colorScheme, .dark)
     }
 }
