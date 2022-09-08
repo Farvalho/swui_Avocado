6d4ff358c83f5bf78399bbc98dcb97d35fb0f4d7
Fabio Carvalho <farvalho@outlook.com>
2022-09-08 16:31:46 +0100

    +Add: SettingsView layout

--- Avocado/Avocado/Presentation/Views/SettingsView.swift
+++ Avocado/Avocado/Presentation/Views/SettingsView.swift
@@ -8,8 +8,114 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    @State private var enableNotification: Bool = true
+    @State private var backgroundRefresh: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center, spacing: 0) {
+            
+            
+            VStack(alignment: .center, spacing: 5) {
+                Image("avocado")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.top)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .shadow(color: Color("ColorBlackTransparentLight"), radius: 8, x: 0, y: 4)
+                
+                Text("Avocados".uppercased())
+                    .font(.system(.title, design: .serif))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("ColorGreenAdaptive"))
+                
+            } //:VStack
+            .padding()
+            
+            Form {
+                // #1
+                Section {
+                    Toggle(isOn: $enableNotification) {
+                        Text("Enable notification")
+                    }
+                    
+                    Toggle(isOn: $backgroundRefresh) {
+                        Text("Background refresh")
+                    }
+                    
+                } header: {
+                    Text("General settings")
+                }
+                
+                // #2
+                Section {
+                    if enableNotification {
+                        HStack {
+                            Text("Product")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("Avocado recipes")
+                            
+                        } //:HStack
+                        HStack {
+                            Text("Compatibility")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("iPhone & iPad")
+                            
+                        } //:HStack
+                        
+                        HStack {
+                            Text("Developer")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("Fabio Carvalho")
+                            
+                        } //:HStack
+                        
+                        HStack {
+                            Text("Designer")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("Robert Petras")
+                            
+                        } //:HStack
+                        
+                        HStack {
+                            Text("Website")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("www.google.com")
+                            
+                        } //:HStack
+                        
+                        HStack {
+                            Text("Version")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("1.0.0")
+                            
+                        } //:HStack
+                        
+                    } else {
+                        HStack {
+                            Text("Personal message")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("Hello world!")
+                        }
+                    }
+                    
+                    
+                    
+                } header: {
+                    Text("Application")
+                }
+                
+            } //:Form
+            
+        } //:VStack
+        .frame(maxWidth: 640)
     }
 }
 
